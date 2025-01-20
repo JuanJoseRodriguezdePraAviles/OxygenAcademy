dfda80bc6818cd667d04594bb6344a7bb38f0833
JuanJoseRodriguezdePraAviles <45094901+JuanJoseRodriguezdePraAviles@users.noreply.github.com>
2025-01-20 11:21:56 +0100

    Price change with currency implemented

--- js/script.js
+++ js/script.js
@@ -273,15 +273,19 @@ faqForm.addEventListener('submit', async()=>{
 const select = document.querySelector('select.currency');
 select.addEventListener('change', async()=>{
 
-    const changeRatio = 1;
+    let changeRatio = 1;
 
     const wordQuery = select.value;
         
     const endpoint = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json`;
 
     let listChangeJSON;
-        
-    listChangeJSON = fetch(endpoint, {cache: 'no-cache'}).then(response => {
+
+    let originalValueBasic = 0;
+    let originalValueProfessional = 25;
+    let originalValuePremium = 60;
+    let unitSymbol = "$";   
+    changeRatio = await fetch(endpoint, {cache: 'no-cache'}).then(response => {
         
         if (response.ok) {
             listChangeJSON = response.json();
@@ -293,8 +297,34 @@ select.addEventListener('change', async()=>{
         
         console.log(networkError.message)
         
-    });
-    console.log(listChangeJSON);
+    }).then(item => {
+        console.log(select.value);
+        if(select.value === 'eur'){
+            unitSymbol="€";
+            return 1/item.eur.usd;
+        } else if(select.value === 'gbp') {
+            unitSymbol="£";
+            return item.eur.gbp;
+        } else {
+            unitSymbol="$"
+            return 1;
+        }
+        
+    })
+    console.log(changeRatio);
+    
+    let newBasicPrice = Math.round(originalValueBasic * changeRatio *100)/100;
+    let newProfessionalPrice = Math.round(originalValueProfessional * changeRatio*100)/100;
+    let newPremiumPrice = Math.round(originalValuePremium * changeRatio*100)/100;
+
+
+
+    document.querySelector('h3.basic').textContent = unitSymbol + newBasicPrice;
+    document.querySelector('h3.professional').textContent = unitSymbol + newProfessionalPrice;
+    document.querySelector('h3.premium').textContent = unitSymbol + newPremiumPrice;
+    
 });
+
+
     
 
